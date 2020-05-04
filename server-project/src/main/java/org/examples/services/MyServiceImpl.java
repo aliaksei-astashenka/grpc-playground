ffecc4d4640da6746407a37be6c40ad432dfb640
aliaksei.astashenka <leffchik69@gmail.com>
2020-05-04 17:53:47 +0300

    change response to get current address

--- server-project/src/main/java/org/examples/services/MyServiceImpl.java
+++ server-project/src/main/java/org/examples/services/MyServiceImpl.java
@@ -6,15 +6,26 @@ import org.examples.lib.HelloReply;
 import org.examples.lib.HelloRequest;
 import org.examples.lib.MyServiceGrpc;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @GrpcService
 public class MyServiceImpl extends MyServiceGrpc.MyServiceImplBase {
 
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
         HelloReply reply = HelloReply.newBuilder()
-                .setMessage("Hello ==> " + request.getName())
+                .setMessage("Hello ==> " + request.getName() + ", My address is ==> " + getAddress())
                 .build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
+    }
+
+    private String getAddress() {
+        try {
+            return InetAddress.getLocalHost().toString();
+        } catch (UnknownHostException e) {
+            return "Unknown:(";
+        }
     }
 }
